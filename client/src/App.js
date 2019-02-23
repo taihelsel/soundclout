8916da64a84653c35e50bf3b47ee269edc231d9b
taihelsel <tai.bayard@gmail.com>
2019-02-23 05:52:40 -0800

    added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+//Components
+import Home from "./components/Home/Home.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      todo
+        <Home />
       </div>
     );
   }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <section>
+                Home Page
+        </section>
+        );
+    }
+}
+
+export default Home;
