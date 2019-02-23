338431ebc4efd5c99ab715979020922754c9c06c
taihelsel <tai.bayard@gmail.com>
2019-02-23 06:26:21 -0800

    added basic style to home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import "./Home.css";
 //Components
 import Nav from "../Nav/Nav.js";
 class Home extends Component {
+    state = {
+        searchText: "",
+    }
+    updateSearchText = (e) => {
+        const text = e.target.value;
+        const _state = this.state;
+        _state.searchText = text;
+        this.setState(_state);
+    }
     render() {
         return (
             <section id="Home">
-                <Nav />
+                <h1 id="home-head">SoundClout</h1>
+                <div id="home-search">
+                    <input type="text" onChange={this.updateSearchText} value={this.state.searchText} />
+                </div>
             </section>
         );
     }
