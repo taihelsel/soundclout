abe7dc89184b7c66fb4ccd2928f5b6c5e25d0dab
taihelsel <tai.bayard@gmail.com>
2019-02-23 05:56:43 -0800

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 //Components
 import Home from "./components/Home/Home.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
     );
   }
 }
