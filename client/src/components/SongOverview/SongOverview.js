3b62c19ef712ee337c1b415181594ec0b6a571fd
taihelsel <tai.bayard@gmail.com>
2019-02-24 03:20:18 -0800

    added song overview component

---
+++ client/src/components/SongOverview/SongOverview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./SongOverview.css";
+class SongOverview extends Component {
+    render() {
+        return (
+            <section id="SongOverview">
+                SongOverview
+            </section>
+        );
+    }
+}
+
+export default SongOverview;
