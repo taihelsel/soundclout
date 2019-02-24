1545f26448528c54cc875c415eb44d4563f2ab00
taihelsel <tai.bayard@gmail.com>
2019-02-24 03:20:00 -0800

    added song overview component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //Components
 import Home from "./components/Home/Home.js";
-
+import SongOverview from "./components/SongOverview/SongOverview.js";
 class App extends Component {
   render() {
     return (
       <Router>
         <div style={{width:"100%",height:"fit-content"}}>
           <Route exact path="/" component={Home} />
+          <Route path="/song" component={SongOverview} />
         </div>
       </Router>
     );
