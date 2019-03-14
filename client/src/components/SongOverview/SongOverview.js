b812691d478a5220b8f3f5d56daab4b95e3c6ad5
taihelsel <tai.bayard@gmail.com>
2019-03-13 18:00:09 -0700

    able to render song data on line graph. need to improve how data is represented though.

--- client/src/components/SongOverview/SongOverview.js
+++ client/src/components/SongOverview/SongOverview.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import "./SongOverview.css";
 //Components
 import LoadingWheel from "../LoadingWheel/LoadingWheel.js";
@@ -7,6 +7,7 @@ import Nav from "../Nav/Nav.js";
 class SongOverview extends Component {
     state = {
         songData: {},
+        graphData: {},
         dataFetched: false,
     }
     componentWillMount() {
@@ -30,8 +31,50 @@ class SongOverview extends Component {
                 const _state = this.state;
                 _state.dataFetched = true;
                 _state.songData = res;
+                _state.graphData = this.generateGraphData(res);
                 this.setState(_state);
             });
+    }
+    generateGraphData = (songData) => {
+        const playsDataset = {
+            label: "Plays",
+            data: [],
+            borderColor: "#3e95cd",
+            fill: false
+        };
+        const likesDataSet = {
+            label: "Likes",
+            data: [],
+            borderColor: "#8e5ea2",
+            fill: false
+        };
+        const commentsDataSet = {
+            label: "Comments",
+            data: [],
+            borderColor: "#3cba9f",
+            fill: false
+        };
+        const graphData = {
+            labels: [], //will contain the timestamps
+            datasets: [playsDataset, likesDataSet, commentsDataSet], //will contain the data (label,color,data history) for each item (likes,comments, etc)
+            options: {
+                title: {
+                    display: true,
+                    text: "Song History"
+                }
+            }
+        };
+        const data = songData.data;
+        for (let i = 0; i < data.length; i++) {
+            let x = data[i];
+            //adding timestamp
+            graphData.labels.push(x.timeStamp);
+            //updating datasets
+            playsDataset.data.push(x.plays);
+            likesDataSet.data.push(x.likes);
+            commentsDataSet.data.push(x.comments);
+        }
+        return graphData;
     }
     render() {
         return (
@@ -40,36 +83,10 @@ class SongOverview extends Component {
                 {this.state.dataFetched ?
                     (<div>
                         {JSON.stringify(this.state.songData)}
-                        <Bar
-                            data={{
-                                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                                datasets: [{
-                                    label: '# of Votes',
-                                    data: [12, 19, 3, 5, 2, 3],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                    borderColor: [
-                                        'rgba(255,99,132,1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)'
-                                    ],
-                                    borderWidth: 1
-                                }]
-                            }}
+                        <Line
+                            data={this.state.graphData}
                             width={100}
                             height={50}
-                            options={{
-                                maintainAspectRatio: false
-                            }}
                         />
                     </div>)
                     : (<LoadingWheel />)
