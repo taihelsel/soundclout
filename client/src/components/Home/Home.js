3f8cdc50d1f6a000f9317471edf1af6142305a50
taihelsel <tai.bayard@gmail.com>
2019-03-15 07:14:16 -0700

    added recently viewed to homepage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,23 +11,38 @@ class Home extends Component {
         _state.searchText = text;
         this.setState(_state);
     }
-    handleSearchSubmit = (e) => {
-        e.preventDefault();
-        const _state = this.state;
-        let url = _state.searchText;
+    formatUrl = (url) => {
         url = url.replace("https://", "");
         url = url.replace("http://", "");
-        const urlArr = url.split("/");
-        window.location.href = `/songdata?u=${urlArr[1]}&s=${urlArr[2]}`;
+        return url.split("/");
     }
+    handleSearchSubmit = (e) => {
+        e.preventDefault();
+        const _state = this.state, url = this.formatUrl(_state.searchText);
+        window.location.href = `/songdata?u=${url[1]}&s=${url[2]}`;
+    }
+    handleHistoryClick = (url) => {
+        url = this.formatUrl(url);
+        window.location.href = `/songdata?u=${url[1]}&s=${url[2]}`;
+    }
+    renderRecentlyViewed = () => (
+        <ul className="song-history">
+            <h1 className="song-history-head">Recently Viewed Songs</h1>
+            {localStorage.getItem("songHistory") !== null ? JSON.parse(localStorage.getItem("songHistory")).map(item =>
+                <li>
+                    <h1 onClick={() => { this.handleHistoryClick(item.url) }}>
+                        {item.title}
+                    </h1>
+                </li>
+            ) : ""}
+        </ul>
+    );
     render() {
         return (
             <section id="Home">
                 <h1 id="home-head">SoundClout</h1>
                 <SongSearch />
-                {
-                    localStorage.getItem("songHistory") !== null ? JSON.parse(localStorage.getItem("songHistory")).map(item => <div>{JSON.stringify(item)}</div>) : ""
-                }
+                {this.renderRecentlyViewed()}
             </section>
         );
     }
