0115b8cc5f897171bf82bbeb932695b504fd1d2e
taihelsel <tai.bayard@gmail.com>
2019-03-18 02:30:47 -0700

    changed time format for timer

--- client/src/components/SongOverview/SongOverview.js
+++ client/src/components/SongOverview/SongOverview.js
@@ -154,7 +154,7 @@ class SongOverview extends Component {
                             <iframe id="song-iframe" scrolling="no" frameborder="no" allow="autoplay" src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${this.state.songData.songId}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`}></iframe>
                         </div>
                         {this.renderGraphs()}
-                        <Timer preLabel={"Checking for update in "} postLabel={" seconds"} startTime={Date.now()} endTime={this.state.songData.lastUpdated + this.state.songData.offsetTimer} />
+                        <Timer preLabel={"Updating in "} postLabel={""} startTime={Date.now()} endTime={this.state.songData.lastUpdated + this.state.songData.offsetTimer} />
                     </div>)
                     : (<LoadingWheel />)
                 }
--- client/src/components/Timer/Timer.js
+++ client/src/components/Timer/Timer.js
@@ -3,7 +3,7 @@ class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            timeToDisplay: 0,
+            timeLeft: 0,
             preLabel: this.props.preLabel || "",
             postLabel: this.props.postLabel || "",
             startTime: this.props.startTime, //expected to be passed down in ms.
@@ -13,28 +13,36 @@ class Timer extends Component {
     componentWillMount = () => {
         this.convertTime();
     }
-    updateTimer = () => {
-        const _state = this.state;
-        if (_state.timeToDisplay > 0) {
-            setTimeout(() => {
-                //CURRENTLY ON  A ONE SECOND INTERVAL
-                _state.timeToDisplay -= 1;
-                this.setState(_state);
-            }, 1000);
-        }
-    }
     convertTime = () => {
         //CURRENTLY CONVERTING TO SECONDS. MAY WANT TO CHANGE IN THE FUTURE.
         const _state = this.state;
         const diff = _state.endTime - _state.startTime;
-        _state.timeToDisplay = Math.ceil(diff / 1000);
+        _state.timeLeft = Math.ceil(diff / 1000);
         this.setState(_state);
+    }
+    updateTimer = () => {
+        const _state = this.state;
+        if (_state.timeLeft > 0) {
+            setTimeout(() => {
+                //CURRENTLY ON  A ONE SECOND INTERVAL
+                _state.timeLeft -= 1;
+                this.setState(_state);
+            }, 1000);
+        }
+    }
+    formatTime = (s) => {
+        //MM:SS format
+        if (s < 1) return "00:00";
+
+        let min = Math.floor(s / 60) < 10 ? "0" + Math.floor(s / 60) : Math.floor(s / 60), sec = s % 60 < 10 ? "0" + s % 60 : s % 60;
+
+        return `00:${min}:${sec}`;
     }
     render() {
         this.updateTimer();
         return (
             <div className="timer-wrapper">
-                <h1 className="timer-label">{this.state.preLabel} {this.state.timeToDisplay} {this.state.postLabel}</h1>
+                <h1 className="timer-label">{this.state.preLabel} {this.formatTime(this.state.timeLeft)} {this.state.postLabel}</h1>
             </div>
         );
     }
