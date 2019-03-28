ce93c39faa57bffd85864aac129af7f9018a26fb
taihelsel <tai.bayard@gmail.com>
2019-03-28 13:51:07 -0700

    converted nav to a functional component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Nav.css";
 //Components
 import SongSearch from "../SongSearch/SongSearch.js";
-class Nav extends Component {
-    handleNavBack = () => {
-        this.props.history.push("/");
-    }
-    render() {
-        return (
-            <nav>
-                <h3 id="nav-back-arrow" onClick={this.handleNavBack}>⬅</h3>
-                <SongSearch history={this.props.history} />
-            </nav>
-        );
-    }
+
+const handleNavBack = history => e => {
+    history.push("/");
+}
+const Nav = ({ history }) => {
+    return (
+        <nav>
+            <h3 id="nav-back-arrow" onClick={handleNavBack(history)}>⬅</h3>
+            <SongSearch history={history} />
+        </nav>
+    );
 }
 export default Nav;
