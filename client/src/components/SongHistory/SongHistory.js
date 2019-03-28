f7bc6a9f3671663602ceafb051319b923e2e7721
taihelsel <tai.bayard@gmail.com>
2019-03-28 14:25:53 -0700

    set onclick to li instead of h1

--- client/src/components/SongHistory/SongHistory.js
+++ client/src/components/SongHistory/SongHistory.js
@@ -5,8 +5,8 @@ const SongHistory = ({ history, handleHistoryClick }) => {
         <ul className="song-history">
             <h1 className="song-history-head">Recently Viewed Songs</h1>
             {JSON.parse(localStorage.getItem("songHistory")).map(item =>
-                <li>
-                    <h1 onClick={handleHistoryClick(history, item.url)}>
+                <li onClick={handleHistoryClick(history, item.url)}>
+                    <h1>
                         {item.title}
                     </h1>
                 </li>
