4bcffa57c4f6fece381c8c8882fe27fa1a78268e
taihelsel <tai.bayard@gmail.com>
2019-03-29 16:59:56 -0700

    resetting search text after submitting form

--- client/src/components/SongSearch/SongSearch.js
+++ client/src/components/SongSearch/SongSearch.js
@@ -7,17 +7,18 @@ const handleSearchInput = updateText => e => {
     const text = e.target.value;
     updateText(text);
 }
-const handleSearchSubmit = (history, searchText) => e => {
+const handleSearchSubmit = (history, searchText, updateText) => e => {
     e.preventDefault();
     let url = searchText;
     url = url.replace("https://", "");
     url = url.replace("http://", "");
     const urlArr = url.split("/");
     history.push(`/songdata/${urlArr[1]}/${urlArr[2]}`);
+    updateText("");
 }
 const SongSearch = ({ history, searchText, updateText }) => {
     return (
-        <form className="song-search" onSubmit={handleSearchSubmit(history, searchText)}>
+        <form className="song-search" onSubmit={handleSearchSubmit(history, searchText, updateText)}>
             <input type="text" onChange={handleSearchInput(updateText)} value={searchText} placeholder="Paste in song url and hit Enter" />
             <input type="submit" style={{ display: "none" }} />
         </form>
